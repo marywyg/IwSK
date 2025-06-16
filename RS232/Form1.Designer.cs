b2ddb2509540c7006cce427aa08548e06391710c
Maria Wyganowska <115419159+marywyg@users.noreply.github.com>
2025-06-16 20:25:49 +0200

    Add RS232 project

---
+++ RS232/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace RS232CommunicationApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.TextBox textBoxReceived;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonRefresh;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.textBoxReceived = new System.Windows.Forms.TextBox();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // comboBoxPorts
+            this.comboBoxPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPorts.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 21);
+
+            // buttonRefresh
+            this.buttonRefresh.Location = new System.Drawing.Point(139, 12);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.Text = "Odśwież";
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+
+            // buttonConnect
+            this.buttonConnect.Location = new System.Drawing.Point(220, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.Text = "Połącz";
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+
+            // textBoxSend
+            this.textBoxSend.Location = new System.Drawing.Point(12, 50);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(283, 20);
+
+            // buttonSend
+            this.buttonSend.Location = new System.Drawing.Point(301, 48);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.Text = "Wyślij";
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+
+            // textBoxReceived
+            this.textBoxReceived.Location = new System.Drawing.Point(12, 85);
+            this.textBoxReceived.Multiline = true;
+            this.textBoxReceived.Name = "textBoxReceived";
+            this.textBoxReceived.ReadOnly = true;
+            this.textBoxReceived.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReceived.Size = new System.Drawing.Size(364, 100);
+
+            // textBoxLog
+            this.textBoxLog.Location = new System.Drawing.Point(12, 195);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(364, 100);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(390, 310);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxReceived);
+            this.Controls.Add(this.textBoxLog);
+            this.Name = "Form1";
+            this.Text = "RS-232 Communication";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ RS232/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace RS232CommunicationApp
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private Stopwatch pingTimer = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshPorts();
+        }
+
+        private void RefreshPorts()
+        {
+            comboBoxPorts.Items.Clear();
+            comboBoxPorts.Items.AddRange(SerialPort.GetPortNames());
+            if (comboBoxPorts.Items.Count > 0)
+                comboBoxPorts.SelectedIndex = 0;
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshPorts();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort = new SerialPort(comboBoxPorts.Text, 9600, Parity.None, 8, StopBits.One);
+                serialPort.Handshake = Handshake.None;
+                serialPort.DataReceived += SerialPort_DataReceived;
+                serialPort.Open();
+                textBoxLog.AppendText($"[INFO] Połączono z {serialPort.PortName}\r\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd połączenia: " + ex.Message);
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                string message = textBoxSend.Text;
+                pingTimer.Restart();
+                serialPort.WriteLine(message);
+                textBoxLog.AppendText($"[TX] {message}\r\n");
+            }
+            else
+            {
+                MessageBox.Show("Nie połączono z portem COM.");
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string received = serialPort.ReadExisting();
+            pingTimer.Stop();
+
+            Invoke(new Action(() =>
+            {
+                textBoxReceived.AppendText(received + "\r\n");
+                textBoxLog.AppendText($"[RX] {received.Trim()} (Ping: {pingTimer.ElapsedMilliseconds} ms)\r\n");
+            }));
+        }
+    }
+}
